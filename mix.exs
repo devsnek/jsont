9db6239ea185959d9f510b610ce46a314609a4a4
Gus Caplan <me@gus.host>
2024-02-26 15:07:56 -0800

    hex publish

--- mix.exs
+++ mix.exs
@@ -7,19 +7,38 @@ defmodule Jsont.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      description: "A blazingly fast JSON library",
+      source_url: "https://github.com/devsnek/jsont",
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      files: [
+        "lib",
+        "LICENSE",
+        "mix.exs",
+        "README.md",
+        "native/jsont_nif/src",
+        "native/jsont_nif/Cargo.toml",
+        "native/jsont_nif/Cargo.lock"
+      ],
+      links: %{"GitHub" => "https://github.com/devsnek/jsont"}
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
   defp deps do
     [
-      {:rustler, "~> 0.30.0", runtime: false},
+      {:rustler, "~> 0.31.0", runtime: false},
       {:stream_data, "~> 0.5", only: [:dev, :test], runtime: false},
       {:snapshy, "~> 0.4", only: [:dev, :test], runtime: false}
     ]
