ba70e2717b09728e151f3ebae19aa0a977c36429
Gus Caplan <me@gus.host>
2024-02-27 12:40:07 -0800

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jsont.MixProject do
   def project do
     [
       app: :jsont,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       description: "A blazingly fast JSON library",
