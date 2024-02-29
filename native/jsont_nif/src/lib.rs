c9248f7c36f2984219f936770f26da7e7faa554d
Gus Caplan <me@gus.host>
2024-02-28 18:31:31 -0800

    wip yield

--- native/jsont_nif/src/lib.rs
+++ native/jsont_nif/src/lib.rs
@@ -1,6 +1,6 @@
 use rustler::{
     types::{atom, tuple::make_tuple, Encoder},
-    Atom, Binary, Env, NewBinary, Term,
+    Atom, Binary, Branch, Env, NewBinary, Term,
 };
 use std::io::Write;
 mod big;
@@ -15,37 +15,34 @@ fn encode<'a>(
     term: Term<'a>,
     bigint_as_string: bool,
     strip_elixir_struct: bool,
-) -> (Atom, Term<'a>) {
+) -> Term<'a> {
     match encoder::encode(env, term, bigint_as_string, strip_elixir_struct) {
         Ok(out) => {
             let mut bin = NewBinary::new(env, out.len());
             if let Err(e) = bin.as_mut_slice().write_all(&out) {
-                return (atom::error(), encode_error(env, e.into()));
+                return encode_error(env, e.into());
             }
             let term = Binary::from(bin).to_term(env);
-            (atom::ok(), term)
+            (atom::ok(), term).encode(env)
         }
-        Err(e) => (atom::error(), encode_error(env, e)),
+        Err(e) => encode_error(env, e),
     }
 }
 
 #[rustler::nif(schedule = "DirtyCpu")]
-fn decode<'a>(env: Env<'a>, term: Term<'a>) -> (Atom, Term<'a>) {
+fn decode<'a>(env: Env<'a>, term: Term<'a>) -> Branch<'a, Term<'a>> {
     let binary = match Binary::from_iolist(term) {
         Ok(binary) => binary,
         Err(e) => {
-            return (atom::error(), encode_error(env, Error::Rustler(e)));
+            return Branch::Stop(encode_error(env, Error::Rustler(e)));
         }
     };
-    match decoder::decode(env, binary) {
-        Ok(term) => (atom::ok(), term),
-        Err(e) => (atom::error(), encode_error(env, e)),
-    }
+    decoder::decode(env, binary, 0, Term::list_new_empty(env))
 }
 
 #[cold]
-fn encode_error<'a>(env: Env<'a>, e: Error<'a>) -> Term<'a> {
-    match e {
+pub(crate) fn encode_error<'a>(env: Env<'a>, e: Error<'a>) -> Term<'a> {
+    let data = match e {
         Error::Tuple(atom, terms) => {
             let terms = [&[atom.encode(env)], terms.as_slice()].concat();
             make_tuple(env, &terms)
@@ -57,7 +54,9 @@ fn encode_error<'a>(env: Env<'a>, e: Error<'a>) -> Term<'a> {
                 .encode(env)
         }
         Error::Rustler(rustler::Error::Term(t) | rustler::Error::RaiseTerm(t)) => t.encode(env),
-    }
+    };
+
+    (atom::error(), data).encode(env)
 }
 
 enum Error<'a> {
